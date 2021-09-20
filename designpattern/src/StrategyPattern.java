0ad80f30cc50fde5510dc0b1009f6cc748d409b5
viskwon <viskwon@gmail.com>
2021-09-21 03:32:29 +0900

    StrategyPattern example

--- designpattern/src/StrategyPattern.java
+++ designpattern/src/StrategyPattern.java
@@ -2,28 +2,33 @@ public class StrategyPattern {
 
     public static void main(String[] args) {
         System.out.println("strategyPattern");
+        Duck duck = new MallardDuck();
+        duck.performFly();
+        duck.setFlyBehavior(new FlyWithWing());
+        duck.performFly();
+
     }
 
 
-    interface FlyBehavior{ // 변하는것 Strategy
+    interface FlyBehavior { // 변하는것 Strategy
         void fly();
     }
 
-    public static class FlyWithWing implements FlyBehavior{ //ConcreteStrategy
+    public static class FlyWithWing implements FlyBehavior { //ConcreteStrategy
         @Override
         public void fly() {
             System.out.println("flywithWing");
         }
     }
 
-    public static class NoFly implements FlyBehavior{ //ConcreteStrategy
+    public static class FlyNo implements FlyBehavior { //ConcreteStrategy
         @Override
         public void fly() {
-            System.out.println("no fly");
+            System.out.println("FlyNo");
         }
     }
 
-    public static class FlyusingRocket implements FlyBehavior{ //ConcreteStrategy
+    public static class FlyusingRocket implements FlyBehavior { //ConcreteStrategy
         @Override
         public void fly() {
             System.out.println("FlyusingRocket");
@@ -31,11 +36,11 @@ public class StrategyPattern {
     }
 
 
-    interface QuackBehavior{// 변하는 것  //strategy
+    interface QuackBehavior {// 변하는 것  //strategy
         void quack();
     }
 
-    public static class Quack implements QuackBehavior{  //ConcreteStrategy
+    public static class Quack implements QuackBehavior {  //ConcreteStrategy
 
         @Override
         public void quack() {
@@ -43,44 +48,46 @@ public class StrategyPattern {
         }
     }
 
-    public static class NoQuack implements QuackBehavior{ //ConcreteStrategy
+    public static class QuackNo implements QuackBehavior { //ConcreteStrategy
 
         @Override
         public void quack() {
-            System.out.println("No Quack");
+            System.out.println("Quack No ");
         }
     }
-    public abstract static class Duck{
+
+    public abstract static class Duck {
 
         FlyBehavior flyBehavior; // object composition 변하는 것들
         QuackBehavior quackBehavior;
 
-        public void setFlyBehavior(FlyBehavior behavior){
+        public void setFlyBehavior(FlyBehavior behavior) {
             flyBehavior = behavior;  //changeable
         }
 
-        public void setQuackBehavior(QuackBehavior behavior){
+        public void setQuackBehavior(QuackBehavior behavior) {
             quackBehavior = behavior;  //changeable
         }
 
         // Delegation
-        public void swim(){} // 변하지 않는 것 .. 공통적임
+        public void swim() {
+        } // 변하지 않는 것 .. 공통적임
 
-        public void performFly(){
+        public void performFly() {
             flyBehavior.fly(); // delegation 동작을 위임함..
         }
-        public void performQuack(){
+
+        public void performQuack() {
             quackBehavior.quack();
         }
     }
 
-    public class MallardDuck extends Duck {
+    public static class MallardDuck extends Duck {
         public MallardDuck() {
-            flyBehavior = new NoFly();
+            flyBehavior = new FlyNo();
             quackBehavior = new Quack();
         }
 
     }
-
 
 }
